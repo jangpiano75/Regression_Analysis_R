c3112cfa0f4ca9522b2e4bf2d2a792c8e17e664d
jangpiano75 <77634279+jangpiano75@users.noreply.github.com>
2021-01-21 18:33:28 +0900

    Update [Mult]Adjusted_multiple_linear_regression.R

--- [Mult]Adjusted_multiple_linear_regression.R
+++ [Mult]Adjusted_multiple_linear_regression.R
@@ -1,53 +1,10 @@
 #https://jangpiano-science.tistory.com/110?category=875432
 
-#
-#code for adjusted multiple linear regression with 2 variables 
-multiple_regre_v_2 = function(X1, X2, Y, x1, x2){
-  
-  options("scipen" = 100)  #지수로 표기될 경우를 대비해 (지수표기 -->숫자표기)
-  
-  y_1<-lm(Y~X1)
-  e1 = resid(y_1) 
-  
-  x_1<-lm(X2~X1)
-  e2 = resid(x_1)
-  
-  estimate_B2 = lm(e1 ~e2)$coefficients[2]
-  
-  y_2<-lm(Y~X2)
-  e11 = resid(y_2)
-  
-  x_2<-lm(X1~X2)
-  e22 = resid(x_2)
-  
-  estimate_B1 = lm(e11 ~ e22)$coefficients[2]
-  
-  estimate_B0 = mean(Y) - estimate_B1*mean(X1) - estimate_B2*mean(X2)
-  
-  y_estimate = estimate_B0 + estimate_B1*x1 + estimate_B2*x2
-  y_estimate
-}
-
-#[STEPS]
-
-X1<-c(3, 3, 4, 5, 5, 6, 6, 6, 7, 7, 8, 9, 10)
-X2<-c(2, 4, 6, 5, 8, 4, 6, 9, 7, 9, 7, 9, 10)
-Y<-c(1, 2, 3, 3, 6, 4, 5, 7, 3, 7, 6, 9, 10)
-
-e1 = resid(lm(Y~X1))
-e2 = resid(lm(X2~X1))
-estimate_B1 = lm(e1 ~e2)$coefficients[2]
-
-e11 = resid(lm(Y~X2))
-e22 = resid(lm(X1~X2))
-estimate_B2 = lm(e11~e22)$coefficients[2]
-
-
-estimate_B0 = mean(Y) - estimate_B1*mean(X1) - estimate_B2*mean(X2)
-
+lm(Y~., data = data_frame)
 
 #[EXAMPLE]
-multiple_regre_v_2(X1, X2, Y, 3, 2)
+data_1 = data.frame(Y, X1, X2, X3)
+lm(Y~.,data_1)
 
 #
 #code for adjusted multiple linear regression with more than 2 variables (generalized code) 
@@ -55,6 +12,7 @@ multiple_regre_v_2(X1, X2, Y, 3, 2)
 estimate_B = list()
 
 adjusted_multi_regre = function(data_frame, Y){
+  options("scipen" = 100)  #지수로 표기될 경우를 대비해 (지수표기 -->숫자표기)
   
   estimate_B0 = mean(Y)
   
