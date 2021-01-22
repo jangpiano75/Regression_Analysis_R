259a906b57b8c2d7e6b07c5771a471ed4532b1ee
jangpiano75 <77634279+jangpiano75@users.noreply.github.com>
2021-01-22 23:50:20 +0900

    Update [Simple]Test,C.I_for_simple_regression_coefficients.R

--- [Simple]Test,C.I_for_simple_regression_coefficients.R
+++ [Simple]Test,C.I_for_simple_regression_coefficients.R
@@ -63,51 +63,55 @@ Test_Regression_Coefficient(X, Y, 0.05)
 
 #CONFIDENCE INTERVAL OF REGRESSION COEFFICIENT
 
-confint(lm(Y~X1), level = 0.05) 
+confint(lm(Y~X1), level = 0.95) 
 
 #[example]
 
 X1 = c(3, 3, 4, 5, 5, 6, 6, 6, 7, 7, 8, 9, 10)  
 Y = c(1, 2, 3, 3, 6, 4, 5, 7, 3, 7, 6, 9, 10)
 
-confint(lm(Y~X1), level = 0.90)
-
+'''
+>confint(lm(Y~X1), level = 0.95)
+                 2.5 %   97.5 %
+(Intercept) -4.3406016 1.234159
+X1           0.6565352 1.525538
+'''
 
 ##--------------------------code for finding confidence interval for B0 and B1
+
 conf_int= function(X, Y, a, B){
   
   sample_mean = mean(X)
   Sxx = (sum((X - sample_mean)^2))
   t_critical = abs(qt(a/2, length(X)-2))
-  
+  estimate_var = sqrt(deviance(lm(Y~X))/(length(X)-2))
+    
   if (B=="B0"){
     
-    estimate_B0 = summary( lm(Y~X) )$coefficients[1]
-    s_e_B0 = summary( lm(Y~X) )$coefficients[3]
+    estimate_B0 = coef(lm(Y~X))[1]
     
-    lower_bound = estimate_B0 - t_critical * (s_e_B0) * (1/length(X) + sample_mean^2/Sxx)^0.5
-    upper_bound = estimate_B0 + t_critical * (s_e_B0) * (1/length(X) + sample_mean^2/Sxx)^0.5
+    lower_bound = estimate_B0 - t_critical * estimate_var * sqrt(1/length(X) + sample_mean^2/Sxx)
+    upper_bound = estimate_B0 + t_critical * estimate_var * sqrt(1/length(X) + sample_mean^2/Sxx)
     
     return(c(lower_bound, upper_bound)) 
   }
   else if (B=="B1"){
     
-    estimate_B1 = summary( lm(Y~X) )$coefficient[2]
-    s_e_B1 = summary( lm(Y~X) )$coefficients[4]
-  
-    lower_bound = estimate_B1 - t_critical * (s_e_B1) * (Sxx)^-0.5
-    upper_bound = estimate_B1 + t_critical * (s_e_B1) * (Sxx)^-0.5
+    estimate_B1 = coef(lm(Y~X))[2]
+    
+    lower_bound = estimate_B1 - t_critical * estimate_var * sqrt(1/Sxx)
+    upper_bound = estimate_B1 + t_critical * estimate_var * sqrt(1/Sxx)
     
     return(c(lower_bound, upper_bound)) 
   }
 }
 
 #[example with the code]
-
-conf_int (X, Y, 0.05, "B0") 
-#-0.1631919  0.5813159
-
-conf_int (X, Y, 0.05, "B1") 
-#1.015865 1.039779
-
-
+'''
+>conf_int(X1, Y, 0.05, "B0")
+(Intercept) (Intercept) 
+  -4.340602    1.234159 
+>conf_int(X1, Y, 0.05, "B1")
+        X         X 
+0.6565352 1.5255376 
+'''
